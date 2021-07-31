b1176148101c9b9bf59f7d539e07e9e715580d4b
stepa <pamallinos@email.cz>
2021-07-31 19:03:15 +0200

    last commit before completly changing packet

--- lib/src/components/message.rs
+++ lib/src/components/message.rs
@@ -1,11 +1,44 @@
+use std::time::SystemTime;
+
 use chrono::{DateTime, NaiveDateTime, Utc};
 
-use crate::Packet;
+use crate::{Packet, settings};
 
+pub enum MessageKind {
+    Empty,
+    Request,
+    Text,
+    File,
+    Unknown,
+} 
 
 pub struct Message {
+    kind: MessageKind,
     author: [u8; 4],
     recipient: [u8; 4], // Sender sends server id, server will match recipient name in metadata packet  
     time: DateTime<Utc>,
     content: Vec<Packet>
+}
+
+impl Message {
+
+    pub fn new(kind: MessageKind, author: [u8; 4]) -> Self {
+
+        let now = SystemTime::now()
+                                    .duration_since(SystemTime::UNIX_EPOCH)
+                                    .unwrap()
+                                    .as_secs() as i64;         
+        let naive_datetime = NaiveDateTime::from_timestamp(now, 0);
+        let time = DateTime::from_utc(naive_datetime, Utc);
+
+        let metadata = Packet::new(kind: Meta)
+
+        Message {
+            kind,
+            author,
+            recipient: settings::SERVER_ID,
+            time,
+            content: Vec::new(),
+        }
+    }
 }
