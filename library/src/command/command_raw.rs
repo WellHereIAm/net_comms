8acaf4f91209a3c7149f26269b6d17c6b1ce9171
stepa <pamallinos@email.cz>
2021-08-15 11:49:07 +0200

    white spaces registered properly

--- library/src/command/command_raw.rs
+++ library/src/command/command_raw.rs
@@ -1,3 +1,4 @@
+use std::iter::FromIterator;
 use std::path::Path;
 
 use utils::input;
@@ -22,23 +23,42 @@ impl CommandRaw {
     where 
         T: std::fmt::Display {
 
-    // Probably better to split at ()
     let cmd = match msg {
         Some(msg) => {
-            let cmd = input(msg).unwrap()
-                                    .split_whitespace()
+            let cmd: Vec<String> = input(msg).unwrap()
+                                    .split_inclusive(" ")
                                     .map(|cmd| {String::from(cmd)})
-                                    .collect::<Vec<String>>();
+                                    .collect();
             CommandRaw{vec: cmd}
         },
         None => {
-            let cmd = input("").unwrap()
-                                    .split_whitespace()
-                                    .map(|cmd| {String::from(cmd)}).collect::<Vec<String>>();
+            let cmd: Vec<String> = input(" ").unwrap()
+                                    .split_inclusive(" ")
+                                    .map(|cmd| {String::from(cmd)})
+                                    .collect();
             CommandRaw{vec: cmd}
         },
     };
+
+    println!("cmd_raw: {:?}", &cmd);
     cmd
+
+    // // Probably better to split at ()
+    // let cmd = match msg {
+    //     Some(msg) => {
+    //         let cmd = input(msg).unwrap()
+    //                                 .split_whitespace()
+    //                                 .map(|cmd| {String::from(cmd)})
+    //                                 .collect::<Vec<String>>();
+    //         CommandRaw{vec: cmd}
+    //     },
+    //     None => {
+    //         let cmd = input("").unwrap()
+    //                                 .split_whitespace()
+    //                                 .map(|cmd| {String::from(cmd)}).collect::<Vec<String>>();
+    //         CommandRaw{vec: cmd}
+    //     },
+    // };
     }
 
     // pub fn vec(&self) -> Vec<u8> {
@@ -49,8 +69,53 @@ impl CommandRaw {
     //     self.vec
     // }
 
-    // This function consumes the whole CommandRaw struct.    
-    pub fn process(self, user: &User) -> Result<Command, NetCommsError> {
+    /// This method consumes the whole CommandRaw struct.    
+    pub fn process(mut self, user: &User) -> Result<Command, NetCommsError> {
+
+        match self.vec.get_mut(0) {
+            Some(cmd) => {
+                let cmd = cmd.replace(" ", "");
+
+                match cmd.as_str() {
+                    "register" => {
+                        // Later solve situations where check returns an Err value.
+                        let user = CommandRaw::check_register(self).unwrap();
+                        return Ok(Command::Register(user))
+                    },
+                    "login" => {
+                        // Later solve situations where check returns an Err value.
+                        let user = CommandRaw::check_login(self).unwrap();
+                        return Ok(Command::Login(user))
+                    },
+                    "y" => {
+                        // Finish check function
+                        match CommandRaw::check_yes(self) {
+                            Ok(_) => return Ok(Command::Yes),
+                            Err(_) => todo!(),
+                            
+                        };
+                    },
+                    "n" => {
+                        // Finish check function
+                        match CommandRaw::check_no(self) {
+                            Ok(_) => return Ok(Command::No),
+                            Err(_) => todo!(),
+                            
+                        };
+                    },
+                    "send" => {
+                        let msg = CommandRaw::check_send(self, &user);
+                        return Ok(msg.unwrap())
+                    },
+                    _ => {
+                        // Maybe throw some error?
+                        println!("Unknown command.");
+                        return Ok(Command::Unknown)
+                    },   
+                }
+            },
+            None => todo!(),
+        }
         // ERROR HANDLING!
         match self.vec[0].as_str() {
             "register" => {
@@ -117,69 +182,79 @@ impl CommandRaw {
         Ok(Command::No)
     }
 
-    fn check_send(cmd: CommandRaw, user: &User) -> Result<Command, NetCommsError> {
+    fn check_send(mut cmd: CommandRaw, user: &User) -> Result<Command, NetCommsError> {
 
+        let mut cmd_iter = cmd.vec.iter();
+        
+        // Used to skip send part, so it´s not added as recipient. Later will use 'if let'
+        match cmd_iter.next() {
+            Some(v) => {
+                dbg!(v);
+            },
+            None => {
+                // Return an error.
+            }
+        }
+
+        // Get all recipients.
         let mut recipients: Vec<String> = Vec::new();
-        let mut recipients_length = 0;
-        let cmd_len = cmd.vec.len();
-
-        match cmd.vec.get(1) {
-            Some(string) => {
-
-                if string.starts_with("(") {
-
-                    let mut is_last = false;
-
-                    for recipient in cmd.vec[1..cmd_len].to_vec() {
-                        
-                        if recipient.ends_with(")") {
+        loop {
+            match cmd_iter.next() {
+                Some(part) => {
+                    if part.as_str() == " " {
+                        continue;
+                    }
+                    
+                    if part.starts_with("(") {                
+        
+                        let mut is_last = false;
+        
+                        if part.ends_with(")") {
                             is_last = true;
                         }
-
-                        let recipient = recipient.replace("(", "");
+        
+                        // I may create some function that will later check if part is using valid symbols only, but I don´t even know what´s valid now.
+                        let recipient = part.replace("(", "");
                         let recipient = recipient.replace(")", "");
-
-                        let recipients_part: Vec<String> = recipient.split(",").map(|rec| rec.to_string()).collect();
-
-                        for recipient in recipients_part {
-                            if recipient.len() > 0 {
-                                recipients.push(recipient);
-                                recipients_length += 1;
-                            }
+                        let recipient = recipient.replace(",", "");
+                        let recipient = recipient.replace(" ", "");
+        
+                        if !recipient.is_empty() {
+                            recipients.push(recipient);
                         }
-
+        
                         if is_last {
                             break;
                         }
-                    }
-                } else {
-                    recipients_length += 1;
-                    recipients.push(cmd.vec[1].clone());
-                }
-            }
-            None => {
-                // Invoke error.
-                todo!();
-            },
-        }
-
-        let does_exist: bool;
-        match cmd.vec.get(recipients_length + 1) {
-            Some(_) => {
-                does_exist = true;
-            },
-            None => {
-                does_exist = false
-            }, // Invoke error
-        }
-
-        let mut cmd_content = String::new();
+                    } else {
+                        // I may create some function that will later check if part is using valid symbols only, but I don´t even know what´s valid now.
+                        let recipient = part.replace("(", "");
+                        let recipient = recipient.replace(")", "");
+                        let recipient = recipient.replace(",", "");
+                        let recipient = recipient.replace(" ", "");
         
-        if does_exist {
-            for mut part in cmd.vec[recipients_length + 1..cmd_len].to_vec() {
-                part.push(' ');
-                cmd_content.push_str(&part);          
+                        if !recipient.is_empty() {
+                            recipients.push(recipient);
+                        }
+                        break;
+                    }
+                },
+                None => {
+                    // Return an IOError as this command was used wrongly.
+                },
             }
+        }
+
+        if recipients.is_empty() {
+            // Return an error.
+        }
+
+        let cmd_content: String = cmd_iter.map(|string| String::from(string)).collect();
+
+        dbg!(&cmd_content);
+
+        if cmd_content.is_empty()  {
+            // Return an error.
         }
 
         let kind: MessageKind;
@@ -189,11 +264,15 @@ impl CommandRaw {
         // Check if the content of command is Path
         if cmd_content.starts_with("|") {
             kind = MessageKind::File;
-            cmd_content = cmd_content.replace("|", "");
+            let cmd_content = cmd_content.replace("|", "");
             let path = Path::new(&cmd_content);
             if path.is_file() {
-                // Rework this for proper Error handling.
-                file_name = Some(path.to_str().unwrap().to_string());    
+                match path.to_str() {
+                    Some(path) => file_name = Some(path.to_string()),
+                    None => {
+                        // Return an error,
+                    }
+                }
             }
         } else {
             kind = MessageKind::Text;
@@ -208,19 +287,67 @@ impl CommandRaw {
             author_id,
             recipients,
             content,
-            file_name, // Should rename this
+            file_name,
         ))
+
+
+        // match cmd.vec.get(1) {
+        //     Some(string) => {
+
+        //         if string.starts_with("(") {
+
+        //             let mut is_last = false;
+
+        //             for recipient in cmd.vec[1..cmd_len].to_vec() {
+                        
+        //                 if recipient.ends_with(")") {
+        //                     is_last = true;
+        //                 }
+
+        //                 let recipient = recipient.replace("(", "");
+        //                 let recipient = recipient.replace(")", "");
+
+        //                 let recipients_part: Vec<String> = recipient.split(",").map(|rec| rec.to_string()).collect();
+
+        //                 for recipient in recipients_part {
+        //                     if recipient.len() > 0 {
+        //                         recipients.push(recipient);
+        //                         recipients_length += 1;
+        //                     }
+        //                 }
+
+        //                 if is_last {
+        //                     break;
+        //                 }
+        //             }
+        //         } else {
+        //             recipients_length += 1;
+        //             recipients.push(cmd.vec[1].clone());
+        //         }
+        //     }
+        //     None => {
+        //         // Invoke error.
+        //         todo!();
+        //     },
+        // }
+
+        // let does_exist: bool;
+        // match cmd.vec.get(recipients_length + 1) {
+        //     Some(_) => {
+        //         does_exist = true;
+        //     },
+        //     None => {
+        //         does_exist = false
+        //     }, // Invoke error
+        // }
+
+        // let mut cmd_content = String::new();
+        
+        // if does_exist {
+        //     for mut part in cmd.vec[recipients_length + 1..cmd_len].to_vec() {
+        //         part.push(' ');
+        //         cmd_content.push_str(&part);          
+        //     }
+        // }
     }
 }
-
-#[derive(Debug)]
-pub struct CommandRawError;
-
-impl std::fmt::Display for CommandRawError {
-
-    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
-        write!(f, "CommandRawError - TODO")
-    }
-}
-
-impl std::error::Error for CommandRawError {}
